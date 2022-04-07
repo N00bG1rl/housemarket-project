48f31caa50f19120c178546069d74f417f1219d6
N00bG1rl <403helen@gmail.com>
2022-04-07 18:55:59 +0300

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+import Navbar from './components/Navbar'
+
 import Explore from './pages/Explore'
 import Offers from './pages/Offers'
 import Profile from './pages/Profile'
@@ -11,14 +13,16 @@ function App() {
 	return (
 		<>
 			<Router>
-				<Routes>
-					<Route path='/' element={<Explore />} />
-					<Route path='/offers' element={<Offers />} />
-					<Route path='/profile' element={<Profile />} />
-					<Route path='/login' element={<Login />} />
-					<Route path='/create-account' element={<CreateAccount />} />
-					<Route path='/forgot-password' element={<ForgotPassword />} />
-				</Routes>
+				<Navbar>
+					<Routes>
+						<Route path='/' element={<Explore />} />
+						<Route path='/offers' element={<Offers />} />
+						<Route path='/profile' element={<Profile />} />
+						<Route path='/login' element={<Login />} />
+						<Route path='/create-account' element={<CreateAccount />} />
+						<Route path='/forgot-password' element={<ForgotPassword />} />
+					</Routes>
+				</Navbar>
 			</Router>
 		</>
 	)
